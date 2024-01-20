9c33600298fd9d92c052f904d6fc93ac2a7752cd
Ben Lewington <ben.lewington91@ntlworld.com>
2024-01-20 09:51:06 +0000

    fixup

--- src/012.c
+++ src/012.c
@@ -8,10 +8,7 @@
 int main(void) {
     long i = 1;
     long tri = 1;
-    long max_factors = 1;
     while (1) {
-        long x = factors_count(tri);
-        if (x > max_factors) max_factors = x;
         if (factors_count(tri) > FIRST_TRI_WITH_DIVISOR_COUNT) break;
         tri = TRI_NUM(i);
         i++;
